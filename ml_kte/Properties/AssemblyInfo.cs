00032e19748332413e21c4e3ae21612d166bc1a3
SDraw <sdraw@protonmail.com>
2021-12-09 19:32:17 +0300

    Automatic replacement of updated embedded dependencies

--- ml_kte/Properties/AssemblyInfo.cs
+++ ml_kte/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("KinectTrackingExtension")]
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 
-[assembly: MelonLoader.MelonInfo(typeof(ml_kte.Main), "KinectTrackingExtension", "1.0.5", "SDraw", "https://github.com/SDraw/ml_mods")]
+[assembly: MelonLoader.MelonInfo(typeof(ml_kte.Main), "KinectTrackingExtension", "1.1.0", "SDraw", "https://github.com/SDraw/ml_mods")]
 [assembly: MelonLoader.MelonGame("VRChat", "VRChat")]
 [assembly: MelonLoader.MelonAdditionalDependencies("VRChatUtilityKit")]
 [assembly: MelonLoader.MelonPlatform(MelonLoader.MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
--- ml_lme/DependenciesHandler.cs
+++ ml_lme/DependenciesHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
 
 namespace ml_lme
 {
@@ -14,27 +15,61 @@ namespace ml_lme
 
         public static void ExtractDependencies()
         {
-            var l_assembly = Assembly.GetExecutingAssembly();
-            var l_assemblyName = l_assembly.GetName().Name;
+            Assembly l_assembly = Assembly.GetExecutingAssembly();
+            string l_assemblyName = l_assembly.GetName().Name;
 
-            foreach(var l_library in ms_libraries)
+            foreach(string l_library in ms_libraries)
             {
+                Stream l_libraryStream = l_assembly.GetManifestResourceStream(l_assemblyName + "." + l_library);
+
                 if(!File.Exists(l_library))
                 {
                     try
                     {
-                        Stream l_readStream = l_assembly.GetManifestResourceStream(l_assemblyName + "." + l_library);
-                        Stream l_writeStream = File.Create(l_library);
-                        l_readStream.CopyTo(l_writeStream);
-                        l_writeStream.Flush();
-                        l_writeStream.Close();
-                        l_readStream.Close();
+                        Stream l_fileStream = File.Create(l_library);
+                        l_libraryStream.CopyTo(l_fileStream);
+                        l_fileStream.Flush();
+                        l_fileStream.Close();
                     }
                     catch(Exception)
                     {
                         MelonLoader.MelonLogger.Error("Unable to extract embedded " + l_library + " library");
                     }
                 }
+                else
+                {
+                    try
+                    {
+                        FileStream l_fileStream = File.Open(l_library, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+                        SHA256 l_hasher = SHA256.Create();
+                        byte[] l_libraryHash = l_hasher.ComputeHash(l_libraryStream);
+                        byte[] l_fileHash = l_hasher.ComputeHash(l_fileStream);
+
+                        for(int i = 0; i < l_libraryHash.Length; i++)
+                        {
+                            if(l_libraryHash[i] != l_fileHash[i])
+                            {
+                                l_fileStream.SetLength(l_libraryStream.Length);
+                                l_fileStream.Position = 0;
+                                l_libraryStream.Position = 0;
+                                l_libraryStream.CopyTo(l_fileStream);
+                                l_fileStream.Flush();
+
+                                MelonLoader.MelonLogger.Msg("Updated " + l_library + " library from embedded one");
+
+                                break;
+                            }
+                        }
+
+                        l_fileStream.Close();
+                    }
+                    catch(Exception)
+                    {
+                        MelonLoader.MelonLogger.Error("Unable to compare/update " + l_library + " library, delete it from game folder manually and restart.");
+                    }
+                }
+
+                l_libraryStream.Close();
             }
         }
     }
